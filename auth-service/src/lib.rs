d1a6f6b097fa26dffd7595f4b3f49cb3fb6eeba9
Vitaliy Kalyniuk <kvit89@gmail.com>
2025-01-06 21:51:56 +0200

    [Sprint 1][Task 5] fix: auth-service add routes, tests

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use axum::{serve::Serve, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
 use tower_http::services::ServeDir;
 
 // This struct encapsulates our application-related logic.
@@ -16,7 +16,12 @@ impl Application {
         // Also, remove the `hello` route.
         // We don't need it at this point!
         let router = Router::new()
-            .nest_service("/", ServeDir::new("assets"));
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/logout", post(logout))
+            .route("/verify-token", post(verify_token));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -30,4 +35,24 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,99 @@
+use auth_service::Application;
+use reqwest::Client;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0")
+            .await
+            .expect("Failed to build app");
+
+        let address = format!("http://{}", app.address.clone());
+
+        // Run the auth service in a separate async task
+        // to avoid blocking the main test thread. 
+        #[allow(clippy::let_underscore_future)]
+        let _ = tokio::spawn(app.run());
+
+        let http_client = Client::new();
+
+        // Create new `TestApp` instance and return it
+        TestApp { address, http_client }
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn signup(&self, email: &str, password: &str, requires2FA: bool) -> reqwest::Response {
+        let payload = format!(
+            r#"{{"email":"{}","password":"{}","requires2FA":{}}}"#,
+            email, password, requires2FA
+        );
+
+        self.http_client
+            .post(&format!("{}/signup", &self.address))
+            .body(payload)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn login(&self, email: &str, password: &str) -> reqwest::Response {
+        let payload = format!(
+            r#"{{"email":"{}","password":"{}"}}"#,
+            email, password
+        );
+
+        self.http_client
+            .post(&format!("{}/login", &self.address))
+            .body(payload)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn verify_2fa(&self, email: &str, login_attempt_id: &str, code_2fa: &str) -> reqwest::Response {
+        let payload = format!(
+            r#"{{"email":"{}","loginAttemptId":"{}","2FACode":"{}"}}"#,
+            email, login_attempt_id, code_2fa
+        );
+
+        self.http_client
+            .post(&format!("{}/verify-2fa", &self.address))
+            .body(payload)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn logout(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/logout", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn verify_token(&self, token: &str) -> reqwest::Response {
+        let payload = format!(
+            r#"{{"token":"{}"}}"#,
+            token
+        );
+
+        self.http_client
+            .post(&format!("{}/verify-token", &self.address))
+            .body(payload)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+}
